be5d0d6e5c3f4793ef4f27aaa4d5c546f19be68e
chen <3150610566@qq.com>
2023-02-13 15:52:00 +0800

    hot-fix commit1

--- service-system/src/main/java/com/system/controller/HelloController.java
+++ service-system/src/main/java/com/system/controller/HelloController.java
@@ -2,10 +2,10 @@ package com.system.controller;
 
 public class HelloController {
     public static void main(String[] args) {
-        System.out.println("hello2 git!!");
-        System.out.println("hello2 git!!");
-        System.out.println("hello2 git!!");
-        System.out.println("hello2 git!!");
-        System.out.println("hello2 git!!");
+        System.out.println("hello4 git!!");
+        System.out.println("hello4 git!!");
+        System.out.println("hello4 git!!");
+        System.out.println("hello4 git!!");
+        System.out.println("hello4 git!!");
     }
 }
