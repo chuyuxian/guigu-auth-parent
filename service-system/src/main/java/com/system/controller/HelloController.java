cb451a626149ca27904b794d66fc8f575efa836a
chen <3150610566@qq.com>
2023-02-14 09:13:50 +0800

    master  commit2

--- service-system/src/main/java/com/system/controller/HelloController.java
+++ service-system/src/main/java/com/system/controller/HelloController.java
@@ -4,8 +4,6 @@ public class HelloController {
     public static void main(String[] args) {
         System.out.println("hello4 git!!");
         System.out.println("hello4 git!!");
-        System.out.println("hello4 git!!");
-        System.out.println("hello4 git!!");
-        System.out.println("hello4 git!!");
+        System.out.println("master test");
     }
 }
